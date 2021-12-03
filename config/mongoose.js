071a067b147297185d0dbc6110abc36a5f7fbb85
Aditya Anshu <89356818+0Apexx0@users.noreply.github.com>
2021-12-03 13:58:35 +0530

    Add files via upload

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+//  require the liberary
+const mongoose = require('mongoose');
+
+//  connect to the datasbase
+mongoose.connect('mongodb://localhost/contact_list_db');
+
+// acquire the connection ( to check if it susccessfull )
+const db = mongoose.connection;
+
+// error
+db.on('error', console.error.bind(console, 'error connecting to db'));
+
+// up and running then print the message
+db.once('open' , ()=>{
+    console.log('successfully connexted to database')
+})
