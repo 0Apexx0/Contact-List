9c7418801e2d9717a800401add7c50c45bbc522b
Aditya Anshu <89356818+0Apexx0@users.noreply.github.com>
2022-03-12 21:53:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 8000
+const port = process.env.PORT || 8000 ;
 const path = require('path');
 const { report } = require('process');
 const app = express();
